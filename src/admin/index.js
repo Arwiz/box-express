39e50da42f3b860f2ef075057753d399438588a4
arwiz <rawat.arvind@hotmail.com>
2020-04-01 22:51:32 +0530

    + Refactor Code

---
+++ src/admin/index.js
@@ -0,0 +1,12 @@
+import organizationRouter from './admin-router';
+import permissionRouter from './permission-router';
+import projectRouter from './project-router';
+import teamRouter from './team-router';
+import {authRouter, userRouter} from "../auth";
+
+export default (app)=> {
+    app.use('/api/v1/projects', projectRouter);
+    app.use('/api/v1/orgs', organizationRouter);
+    app.use('/api/v1/teams', teamRouter);
+    app.use('/api/v1/permissions', permissionRouter);
+};
--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 import express from 'express';
 import errorHandler from "./shared/middleware/errorHandler";
 import { authRouter, userRouter} from "./auth";
-import  adminRouter from "./admin/admin-router";
+import adminRouts from "./admin";
 
 import cors from 'cors'
 const app = new express();
 
 // Body Parser
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 // @desc Health Check API
 app.get('/', function (req, res) {
     res.send('Health Check Fine')
 });
 
-
-// Routing Handling
-// app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/users', userRouter);
-app.use('/api/v1', adminRouter);
-
+// Set up Admin routes
+adminRouts(app);
 
 // Add Error Middleware
 app.use(errorHandler);
